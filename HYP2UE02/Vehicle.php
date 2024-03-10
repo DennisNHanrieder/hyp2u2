e9143c2d72dce1e6c8d2556ea03a0c4264fb60e3
Wolfgang Hochleitner <wolfgang@hochleitner.cc>
2024-03-10 17:30:25 +0100

    Initial commit

---
+++ HYP2UE02/Vehicle.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace HYP2UE02;
+
+class Vehicle
+{
+    // TODO: Create properties for color, speed and maximum speed with different visibility
+    // TODO: Create a constructor that initializes the color, speed, and maximum speed
+    // TODO: Create two methods that accelerate the vehicle and return the current speed
+}
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php
+
+// This is the main file (and only file) that will be directly called in the browser
+// All classes in the HYP2UE02 folder are used in this file directly or indirectly
+
+// TODO: require (or require_once) the necessary classes so their content is available here
+// TODO: Create objects, access properties and methods, and echo the results
